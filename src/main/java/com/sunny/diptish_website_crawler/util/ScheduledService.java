e1b9578a2e2a92b4d9d9d43999fb0b4c8371c72b
Prajyot <prajyot.jagtap@outlook.com>
2025-02-24 16:25:05 -0600

    Encrypting message

--- src/main/java/com/sunny/diptish_website_crawler/util/ScheduledService.java
+++ src/main/java/com/sunny/diptish_website_crawler/util/ScheduledService.java
@@ -8,6 +8,7 @@ public class ScheduledService {
 
     @Scheduled(fixedRate = 10000, initialDelay = 30000)
     private void printHappyMessage(){
-        System.out.println("Diptish 1 number chutiya hai..!!  :) :) ");
+        String message = "): ):  !!..iah ayituhc rebmun 1 hsitpiD";
+        System.out.println(new StringBuffer(message).reverse());
     }
 }
