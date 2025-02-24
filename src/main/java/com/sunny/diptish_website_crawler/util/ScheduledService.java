1dccdaabf9261c25bd171ac7d9b616a55a010849
Prajyot <prajyot.jagtap@outlook.com>
2025-02-24 15:58:00 -0600

    Updated schedule for happy message

--- src/main/java/com/sunny/diptish_website_crawler/util/ScheduledService.java
+++ src/main/java/com/sunny/diptish_website_crawler/util/ScheduledService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ScheduledService {
 
-    @Scheduled(fixedRate = 20000)
+    @Scheduled(fixedRate = 10000, initialDelay = 30000)
     private void printHappyMessage(){
         System.out.println("Diptish 1 number chutiya hai..!!  :) :) ");
     }
